8cb02f74e4c6c98f8b08f507ebb723227e02cedd
Sidd Parikh <siddp15@gmail.com>
2015-12-24 18:17:59 -0500

    first commit

--- src/familyfued/Familyfued.java
+++ src/familyfued/Familyfued.java
@@ -15,6 +15,8 @@ public class Familyfued {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        System.out.println("hello world");
         // TODO code application logic here
     }
     
